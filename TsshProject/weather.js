54192b83a4e1ac06cf9629a26543810857fb18a7
AJI0417 <76050035+AJI0417@users.noreply.github.com>
2022-06-07 14:38:30 +0800

    Version 1.1.1

--- TsshProject/weather.js
+++ TsshProject/weather.js
@@ -2,6 +2,8 @@ function searchOnclick() {
   let searchInput = document.getElementById("searchValue");
   let searchValue = searchInput.value;
   searchValue = searchValue.replace("台", "臺");
+  let result;
+  let render;
   const API = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-B940D0C9-3CF7-4EB5-8E77-500932B41675&elementName=Wx&elementName=PoP&elementName=MaxT&locationName=${searchValue}`;
   const locationName = document.getElementById("locationName"),
     Wx = document.getElementById("Wx"),
@@ -29,8 +31,6 @@ function searchOnclick() {
     "馬祖市",
   ];
   let check = location.includes(searchValue);
-  let result;
-  let render;
   fetch(API)
     .then((res) => {
       return res.json();
@@ -50,3 +50,37 @@ function searchOnclick() {
       }
     });
 }
+
+$(function () {
+  let city = [
+    "基隆縣",
+    "臺北市",
+    "台北市",
+    "新北市",
+    "桃園市",
+    "新竹縣",
+    "新竹市",
+    "苗栗縣",
+    "臺中市",
+    "台中市",
+    "彰化縣",
+    "南投縣",
+    "雲林縣",
+    "嘉義縣",
+    "嘉義市",
+    "臺南市",
+    "台南市",
+    "高雄市",
+    "屏東縣",
+    "宜蘭縣",
+    "花蓮縣",
+    "臺東縣",
+    "台東縣",
+    "澎湖縣",
+    "金門縣",
+    "連江縣",
+  ];
+  $("#searchValue").autocomplete({
+    source: city,
+  });
+});
