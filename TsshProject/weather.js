81ed7cee67021686ff6e9e0760967f33691b72e6
AJI0417 <76050035+AJI0417@users.noreply.github.com>
2022-06-13 23:44:26 +0800

    Add files via upload

--- TsshProject/weather.js
+++ TsshProject/weather.js
@@ -1,42 +1,51 @@
 function searchOnclick() {
+  let date = new Date();
   let searchInput = document.getElementById("searchValue");
   let searchValue = searchInput.value;
   searchValue = searchValue.replace("台", "臺");
   let result;
   let render;
+  document.getElementById("time").innerHTML = date;
   const API = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-B940D0C9-3CF7-4EB5-8E77-500932B41675&elementName=Wx&elementName=PoP&elementName=MaxT&locationName=${searchValue}`;
   const locationName = document.getElementById("locationName"),
     Wx = document.getElementById("Wx"),
     PoP = document.getElementById("PoP"),
-    MaxT = document.getElementById("MaxT"),
-    time = document.getElementById("time");
+    MaxT = document.getElementById("MaxT");
   const location = [
-    "基隆縣",
-    "臺北縣",
-    "新北縣",
-    "桃園縣",
-    "苗栗市",
-    "臺中縣",
-    "南投市",
-    "雲林市",
-    "彰化市",
-    "南投市",
-    "臺南縣",
-    "高雄縣",
-    "屏東市",
-    "宜蘭市",
-    "花蓮市",
-    "台東市",
-    "馬祖縣",
-    "馬祖市",
+    "基隆市",
+    "臺北市",
+    "台北市",
+    "新北市",
+    "桃園市",
+    "新竹縣",
+    "新竹市",
+    "苗栗縣",
+    "臺中市",
+    "台中市",
+    "彰化縣",
+    "南投縣",
+    "雲林縣",
+    "嘉義縣",
+    "嘉義市",
+    "臺南市",
+    "台南市",
+    "高雄市",
+    "屏東縣",
+    "宜蘭縣",
+    "花蓮縣",
+    "臺東縣",
+    "台東縣",
+    "澎湖縣",
+    "金門縣",
+    "連江縣",
   ];
-  let check = location.includes(searchValue);
+  let check = location.indexOf(searchValue);
   fetch(API)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      if (searchValue == "" || check) {
+      if (searchValue == "" || check == -1) {
         alert("請輸入縣市或正確的縣市名稱");
       } else {
         searchInput.value = "";
@@ -46,7 +55,6 @@ function searchOnclick() {
         Wx.innerHTML = render[0].time[0].parameter.parameterName;
         PoP.innerHTML = render[1].time[0].parameter.parameterName + "%";
         MaxT.innerHTML = render[2].time[0].parameter.parameterName + "°C";
-        time.innerHTML = render[0].time[0].startTime;
       }
     });
 }
